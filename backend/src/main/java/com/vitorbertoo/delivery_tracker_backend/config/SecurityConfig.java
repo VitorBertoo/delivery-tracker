5c87b51e806e54d589b5142e7d62e3680e9ccfeb
Vitor Berto <vitorberto.dev@gmail.com>
2026-08-02 22:33:47 -0300

    docs: added swagger docs

--- backend/src/main/java/com/vitorbertoo/delivery_tracker_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/vitorbertoo/delivery_tracker_backend/config/SecurityConfig.java
@@ -28,7 +28,12 @@ public class SecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         auth ->
-                                auth.requestMatchers("/health", "/auth/**")
+                                auth.requestMatchers(
+                                                "/health",
+                                                "/auth/**",
+                                                "/swagger-ui/**",
+                                                "/swagger-ui.html",
+                                                "/v3/api-docs/**")
                                         .permitAll()
                                         .anyRequest()
                                         .authenticated())
