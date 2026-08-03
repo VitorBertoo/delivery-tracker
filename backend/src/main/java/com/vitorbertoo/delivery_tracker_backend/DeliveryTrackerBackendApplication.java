f0b405a4d08924aa27a189fa09003fd0ac4dd00e
Vitor Berto <vitorberto.dev@gmail.com>
2026-08-02 21:06:26 -0300

    feat: init backend app

---
+++ backend/src/main/java/com/vitorbertoo/delivery_tracker_backend/DeliveryTrackerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.vitorbertoo.delivery_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryTrackerBackendApplication.class, args);
+    }
+}
